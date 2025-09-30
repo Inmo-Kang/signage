a7d7f8334d8f1bc50c3535dd77ffd7535c1dd1e7
Inmo-Kang <kangim86@gmail.com>
2025-09-30 10:02:22 +0900

    font_20250930

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,4 +14,21 @@ export default defineNuxtConfig({
   css: [ 
     '~/styles/global.css'
   ],
+    // 👇 @nuxt/fonts 설정 추가
+  fonts: {
+    families: [
+      // Pretendard 폰트 설정
+      { 
+        name: 'Pretendard', 
+        provider: 'local',
+        src: '/fonts/Pretendard-*.subset.woff' // 와일드카드를 사용해 여러 웨이트를 한 번에 등록
+      },
+      // Material Icons 폰트 설정
+      { 
+        name: 'Material Icons Outlined', 
+        provider: 'local',
+        src: '/fonts/MaterialIconsOutlined-Regular.otf'
+      }
+    ]
+  },
 })
